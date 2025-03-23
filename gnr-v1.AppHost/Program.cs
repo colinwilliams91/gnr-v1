2a654b1c6b196e590c82fc2c1bae9653ebc4dcf6
Colin Williams <colin.williams.dev@gmail.com>
2025-03-23 00:34:59 -0500

    :memo:: to connect to existing redis instance, builder method and url docs

--- gnr-v1.AppHost/Program.cs
+++ gnr-v1.AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// To connect to existing Redis instance use the following method: builder.AddConnectionString
+// See: https://learn.microsoft.com/en-us/dotnet/aspire/fundamentals/app-host-overview#reference-existing-resources
 var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.gnr_v1_ApiService>("apiservice");
