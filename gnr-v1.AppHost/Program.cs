0fd462f105c989ada8e3af6df1e37826490c7b57
Colin Williams <colin.williams.dev@gmail.com>
2025-03-23 23:47:32 -0500

    :bug:: npm script start -> dev for vite

--- gnr-v1.AppHost/Program.cs
+++ gnr-v1.AppHost/Program.cs
@@ -6,6 +6,16 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.gnr_v1_ApiService>("apiservice");
 
+// TODO: update scriptName: "dev" arg when UI.package.json.scripts.start (prod) script exists
+builder.AddNpmApp("reacttypescript", "../gnr-v1.UI", "dev")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("BROWSER", "none")
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
 builder.AddProject<Projects.gnr_v1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
